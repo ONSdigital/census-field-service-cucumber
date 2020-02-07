c1f421228fc7d7fb552ee3bc0962ce7bd617faa4
cookel2 <eleanor.cook@ons.gov.uk>
2020-02-07 15:14:50 +0000

    remove sleep that had been added for debugging purposes

--- src/test/java/uk/gov/ons/ctp/integration/fieldsvccucumber/cucSteps/sso/TestSSOFieldService.java
+++ src/test/java/uk/gov/ons/ctp/integration/fieldsvccucumber/cucSteps/sso/TestSSOFieldService.java
@@ -183,13 +183,6 @@ public class TestSSOFieldService extends SpringIntegrationTest {
   @Then("the EQ launch event is triggered")
   public void the_EQ_launch_event_is_triggered() {
 
-    try {
-      log.info("Sleep for 60 seconds to give the 404 error page time to appear");
-      Thread.sleep(60000);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
     String currentURL = driver.getCurrentUrl();
     String pageSource = driver.getPageSource();
     String textToFind = "<div class=\"wrapper\">";
